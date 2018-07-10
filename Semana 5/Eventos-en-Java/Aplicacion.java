eea794644007de54ae086aade806c319a8c6ac7f
yulia2509 <36020622+yulia2509@users.noreply.github.com>
2018-07-10 09:34:04 -0500

    Create Aplicacion.java

---
+++ Semana 5/Eventos-en-Java/Aplicacion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventosjava;
+
+/**
+ *
+ * @author Yulia
+ */
+public class Aplicacion {
+ public static void main(String args[]) {
+  Ventana ventana;
+  ventana = new Ventana();
+  ventana.setVisible(true);
+ }
+}
